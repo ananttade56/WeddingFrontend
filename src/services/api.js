7fac9374b8b5adc3291a4f2d9ba31dfa8904653d
ananttade56 <ananttade56@gmail.com>
2026-05-29 14:12:18 +0530

    Final code

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000", // your backend url
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
